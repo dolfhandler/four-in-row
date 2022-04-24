dee46fa4025385578b238018121b0d49149ca992
dolfhandler <dolfhandler@gmail.com>
2022-04-23 23:26:39 -0500

    Se agregan los graficos propios del juego y se mejora algo de logica en el mismo.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,64 @@
 const WIDTH = 800,
 		HEIGHT = 700,
 		FPS = 60,
-		SHIP_WINNER = 4;
+		CHIP_WINNER = 4;
 		MILISECONDS_IN_A_SECONDS = 1000
 		TILE_WIDTH = 75,
 		TILE_HEIGHT = 75;
-var img0 = new Image();
-var img1 = new Image();
-var img2 = new Image();
-var img3 = new Image();
-img0.src = 'image/central-space-back.png'; 
-img1.src = 'image/ship1.png'; 
-img2.src = 'image/ship2.png'; 
-img3.src = 'image/central-space.png'; 
+		
+const X_CORNER_TOP_LEFT = 0,
+	Y_CORNER_TOP_LEFT = 0,
+	
+	X_TOP_CENTER = TILE_WIDTH * 1,
+	Y_TOP_CENTER = 0,
+	
+	X_CORNER_TOP_RIGHT = TILE_WIDTH * 2,
+	Y_CORNER_TOP_RIGHT = 0,
+	
+	X_LEFT_CENTER = 0,
+	Y_LEFT_CENTER = TILE_HEIGHT * 1,
+	
+	X_CENTER_CENTER = TILE_WIDTH * 1,
+	Y_CENTER_CENTER = TILE_HEIGHT * 1
+	
+	X_RIGHT_CENTER = TILE_WIDTH * 2,
+	Y_RIGHT_CENTER = TILE_HEIGHT * 1
+	
+	X_CORNER_BOTTOM_LEFT = 0,
+	Y_CORNER_BOTTOM_LEFT = TILE_HEIGHT * 2,
+	
+	X_BOTTOM_CENTER = TILE_WIDTH * 1,
+	Y_BOTTOM_CENTER = TILE_HEIGHT * 2,
+	
+	X_CORNER_BOTTOM_RIGHT = TILE_WIDTH * 2,
+	Y_CORNER_BOTTOM_RIGHT = TILE_HEIGHT * 2,
+	
+	X_BACK_STAGE = TILE_WIDTH * 3,
+	Y_BACK_STAGE = TILE_HEIGHT * 2
+	
+	X_RED_CHIP = TILE_WIDTH * 4,
+	Y_RED_CHIP = 0,
+	
+	X_YELLOW_CHIP = TILE_WIDTH * 3,
+	Y_YELLOW_CHIP = 0
+	
+	X_RED_CHIP_WIN = TILE_WIDTH * 4,
+	Y_RED_CHIP_WIN = TILE_HEIGHT * 1,
+	
+	X_YELLOW_CHIP_WIN = TILE_WIDTH * 3,
+	Y_YELLOW_CHIP_WIN = TILE_HEIGHT * 1;
+		
+var tileMap = new Image();
+tileMap.src = 'image/tileMap.png';
 
+var finish = 0;
 var canvas = document.querySelector('#canvas');
 var ctx = canvas.getContext('2d');
 var turn = 0;
 var rowEmpty = [0,0,0,0,0,0,0];
 var fourInRow = [0,0,0,0];
 var fourInLine = [];
-	
+var chipWinnerCollection = [];
 var board = [
 	[0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0],
@@ -30,19 +68,45 @@ var board = [
 	[0,0,0,0,0,0,0],
 	[1,1,1,1,1,1,1],
 ];
+var WIDTH_LIMIT = board[0].length - 1,
+	HEIGHT_LIMIT = board.length - 2;
+	
+var soundChipHit = new Howl({
+	src: ['sound/chipHit2.wav'],
+	loop: false
+});
+
+var soudWinner = new Howl({
+	src: ['sound/winner.wav'],
+	loop: false
+});
+
+/**
+ * Botones
+ */
+var btnStartGame = document.querySelector('#btnStartGame');
+
 
 document.addEventListener('DOMContentLoaded', handlerDomContentLoadedEvent);
 canvas.addEventListener('click', handlerClickEvent);
+btnStartGame.addEventListener('click', handlerClickBtnStartGame);
 
 function handlerDomContentLoadedEvent() {
 	setInterval(mainLoop, MILISECONDS_IN_A_SECONDS/FPS);
 }
 
 function handlerClickEvent(e) {
+	if(finish == 1) return;
+	
 	let chip = new Chip();
-	chip.isInsideBound(e.x, e.y);
+	chip.isInsideBound(e.offsetX, e.offsetY);
+	soundChipHit.play();
 	chips.add(chip);
 	turn++;
+}
+
+function handlerClickBtnStartGame() {
+	restartGame();
 }
 
 function mainLoop() {
@@ -53,6 +117,7 @@ function mainLoop() {
 	chips.draw();
 	chips.goDown();
 	stage.drawFrontStage();
+	showChipWinner();
 }
 
 function clearCanvas() {
@@ -65,8 +130,62 @@ var Stage = function() {
 	this.drawFrontStage = function() {
 		for(let y = 0; y < board.length; y++) {
 			for(let x = 0; x < board[y].length; x++) {
-				if(board[y][x] != 1)
-					ctx.drawImage(img3, (x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT);
+				if(board[y][x] == 1) continue;
+					
+				if(x == 0 && y == 0)
+					ctx.drawImage(
+						tileMap, X_CORNER_TOP_LEFT, Y_CORNER_TOP_LEFT, TILE_WIDTH, TILE_HEIGHT, 
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
+				if((x > 0  && y == 0) && x < WIDTH_LIMIT)
+					ctx.drawImage(
+						tileMap, X_TOP_CENTER, Y_TOP_CENTER, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
+				if(y == 0 && x == WIDTH_LIMIT)
+					ctx.drawImage(
+						tileMap, X_CORNER_TOP_RIGHT, Y_CORNER_TOP_RIGHT, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
+				if((y > 0  && y < HEIGHT_LIMIT) && x == 0)
+					ctx.drawImage(
+						tileMap, X_LEFT_CENTER, Y_LEFT_CENTER, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
+				if((x > 0  && x < WIDTH_LIMIT) && (y > 0 && y < HEIGHT_LIMIT))
+					ctx.drawImage(
+						tileMap, X_CENTER_CENTER, Y_CENTER_CENTER, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
+				if((y > 0  && y < HEIGHT_LIMIT) && x == WIDTH_LIMIT)
+					ctx.drawImage(
+						tileMap, X_RIGHT_CENTER, Y_RIGHT_CENTER, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+					
+				if(x == 0 && y == HEIGHT_LIMIT)
+					ctx.drawImage(
+						tileMap, X_CORNER_BOTTOM_LEFT, Y_CORNER_BOTTOM_LEFT, TILE_WIDTH, 
+						TILE_HEIGHT, (x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
+				if((x > 0  && x < WIDTH_LIMIT) && y == HEIGHT_LIMIT)
+					ctx.drawImage(
+						tileMap, X_BOTTOM_CENTER, Y_BOTTOM_CENTER, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
+				if(y == HEIGHT_LIMIT && x == WIDTH_LIMIT)
+					ctx.drawImage(
+						tileMap, X_CORNER_BOTTOM_RIGHT, Y_CORNER_BOTTOM_RIGHT, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				
 			}
 		}
 	}
@@ -74,25 +193,30 @@ var Stage = function() {
 	this.drawBackStage = function() {
 		for(let y = 0; y < board.length; y++) {
 			for(let x = 0; x < board[y].length; x++) {
-				if(board[y][x] != 1)
-				ctx.drawImage(img0, (x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT);
+				if(board[y][x] == 1) continue;
+				
+				ctx.drawImage(
+					tileMap, X_BACK_STAGE, Y_BACK_STAGE, TILE_WIDTH, TILE_HEIGHT,
+					(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+				);
 			}
 		}
 	}
 	
 	this.draw = function() {
 		for(let y = 0; y < board.length; y++) {
-			for(let x = 0; x < board[y].length; x++) {
-				let color = '#0f0';
-				if(board[y][x] == 1) {
-					color = '#00f';
-				} else if(board[y][x] == 2) {
-					ctx.drawImage(img1, (x + 1) * TILE_WIDTH, (y + 1) * TILE_HEIGHT);
-				} else if(board[y][x] == 3) {
-					ctx.drawImage(img2, (x + 1) * TILE_WIDTH, (y + 1) * TILE_HEIGHT);
-				} else {
+			for(let x = 0; x < board[y].length && board[y][x] != 1; x++) {
 					
-				}
+				if(board[y][x] == 2) 
+					ctx.drawImage(
+						tileMap, X_YELLOW_CHIP, Y_YELLOW_CHIP, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
+				if(board[y][x] == 3)
+					ctx.drawImage(
+						tileMap, X_RED_CHIP, Y_RED_CHIP, TILE_WIDTH, TILE_HEIGHT,
+						(x+1) * TILE_WIDTH, (y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+					);
 			}
 		}			
 	}
@@ -105,7 +229,6 @@ var Chip = function() {
 	this.delay = 2;
 	this.isPosibleDraw = false;
 	this.isHit = false;
-	this.image = img1;
 	
 	this.isInsideBound = function(ex, ey) {
 		for(let i = 0; i < board.length+1; i++) {
@@ -132,8 +255,16 @@ var Chip = function() {
 		if(!this.isPosibleDraw)
 			return;
 		
-		this.image = turn % 2 == 0 ? img1 : img2;
-		ctx.drawImage(this.image, (this.x + 1) * TILE_WIDTH, (this.y + 1) * TILE_HEIGHT);
+		if(turn % 2 == 0)
+			ctx.drawImage(
+				tileMap, X_YELLOW_CHIP, Y_YELLOW_CHIP, TILE_WIDTH, TILE_HEIGHT,
+				(this.x+1) * TILE_WIDTH, (this.y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+			);
+		else
+			ctx.drawImage(
+				tileMap, X_RED_CHIP, Y_RED_CHIP, TILE_WIDTH, TILE_HEIGHT,
+				(this.x+1) * TILE_WIDTH, (this.y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+			);
 	}
 	
 	this.isHitting = function() {
@@ -147,6 +278,7 @@ var Chip = function() {
 	
 	this.goDown = function() {
 		if(this.isHitting()) {
+			//soundChipHit.play();
 			return;
 		}
 		
@@ -163,47 +295,51 @@ var Chip = function() {
 		let pivot = board[y][x];
 		if(this.validateHorizontal(pivot, x, y)) {
 			console.log("GANASTE EN HORIZONTAL!!!!!");
-		console.log(fourInLine);
+			console.log(fourInLine);
+			fillChipWinnerCollection(pivot);
 			return;
 		}
 		
 		fourInLine = [];
 		if(this.validateDiagonalRight(pivot, x, y)) {
 			console.log("GANASTE EN DIAGONAL DERECHA!!!!!");
-		console.log(fourInLine);
+			console.log(fourInLine);
+			fillChipWinnerCollection(pivot);
 			return;
 		}
 		
 		fourInLine = [];
 		if(this.validateDiagonalLeft(pivot, x, y)) {
 			console.log("GANASTE EN DIAGONAL IZQUIERDA!!!!!");
-		console.log(fourInLine);
+			console.log(fourInLine);
+			fillChipWinnerCollection(pivot);
 			return;
 		}
 		
 		fourInLine = [];
 		if(this.validateVertical(pivot, x, y)) {
 			console.log("GANASTE EN VERTICAL!!!!!");
-		console.log(fourInLine);
+			console.log(fourInLine);
+			fillChipWinnerCollection(pivot);
 			return;
 		}
 	}
 	
-	this.validateHorizontal = function(ship, x, y) {
+	this.validateHorizontal = function(chip, x, y) {
 		let saveX = x, saveY = y;
-		let base = fourInRow.map(x => ship);
+		let base = fourInRow.map(x => chip);
 		let horizontal = [];
 		
 		for(let i = 0; i < 4 && (x < board[0].length); i++) {
-			if(board[y][x] != ship) break;
+			if(board[y][x] != chip) break;
 			
 			fourInLine.push({x: x, y: y});
 			horizontal.splice(i,0, board[y][x]);
 			x++;
 		}
 		x = saveX-1;
-		for(let i = 0; i < 4 && (x > 0); i++) {
-			if(board[y][x] != ship) break;
+		for(let i = 0; i < 4 && (x >= 0); i++) {
+			if(board[y][x] != chip) break;
 			
 			fourInLine.push({x: x, y: y});
 			horizontal.splice(i,0, board[y][x]);
@@ -213,11 +349,11 @@ var Chip = function() {
 		return findSubArrayInArray(horizontal, base);
 	}
 	
-	this.validateVertical = function(ship, x, y) {
-		let base = fourInRow.map(fir => ship);
+	this.validateVertical = function(chip, x, y) {
+		let base = fourInRow.map(fir => chip);
 		let vertical = [];
 		
-		for(let i = 0; i < 4 && (y < board[0].length - 1); i++) {
+		for(let i = 0; i < 4 && (y < WIDTH_LIMIT); i++) {
 			fourInLine.push({x: x, y: y});
 			vertical.splice(i,0, board[y][x]);
 			y++;
@@ -226,8 +362,8 @@ var Chip = function() {
 		return findSubArrayInArray(vertical, base);
 	}
 	
-	this.validateDiagonalRight = function(ship, x, y) {
-		let base = fourInRow.map(a => ship);
+	this.validateDiagonalRight = function(chip, x, y) {
+		let base = fourInRow.map(a => chip);
 		let diagonalRight = [];
 		
 		for(let i = 0; i < 4 && (x < board[0].length && y < board.length - 1); i++) {
@@ -239,14 +375,13 @@ var Chip = function() {
 		return findSubArrayInArray(diagonalRight, base);
 	}
 		
-	this.validateDiagonalLeft = function(ship, x, y) {
-		//let saveX = x, saveY = y;
-		let base = fourInRow.map(a => ship);
+	this.validateDiagonalLeft = function(chip, x, y) {
+		let base = fourInRow.map(a => chip);
 		let diagonalLeft = [];
 		
 		//board.length - 1 se le resta uno para que no tenga en cuenta
 		//la fila de tope 
-		for(let i = 0; i < 4 && (x > 0 && y < board.length - 1); i++) {
+		for(let i = 0; i < 4 && (x >= 0 && y < board.length - 1); i++) {
 			fourInLine.push({x: x, y: y});
 			diagonalLeft.splice(i,0, board[y][x]);
 			x--; y++;
@@ -280,16 +415,63 @@ var ChipCollection = function() {
 	}
 }
 
+var ChipWinner = function(x, y, chip) {
+	this.chip = chip;
+	this.x = x;
+	this.y = y;
+	
+	this.drawChipIlumination = function() {
+		if(this.chip == 2)
+			ctx.drawImage(
+				tileMap, X_YELLOW_CHIP_WIN, Y_YELLOW_CHIP_WIN, TILE_WIDTH, TILE_HEIGHT,
+				(this.x+1) * TILE_WIDTH, (this.y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+			);
+		else
+			ctx.drawImage(
+				tileMap, X_RED_CHIP_WIN, Y_RED_CHIP_WIN, TILE_WIDTH, TILE_HEIGHT,
+				(this.x+1) * TILE_WIDTH, (this.y+1) * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT
+			);
+	}
+	
+}
+
 var stage = new Stage();
 var chips = new ChipCollection();
 
+function fillChipWinnerCollection(chipWinner) {
+	soudWinner.play();
+	finish = 1;
+	chipWinnerCollection = fourInLine.map(position => new ChipWinner(position.x, position.y, chipWinner));
+}
 
+function showChipWinner() {
+	for(let chipWinner of chipWinnerCollection) {
+		chipWinner.drawChipIlumination();
+	}
+}
 
-
-
-
-
-
+function restartGame() {
+	finish = 0;
+    canvas = document.querySelector('#canvas');
+    ctx = canvas.getContext('2d');
+    turn = 0;
+    rowEmpty = [0,0,0,0,0,0,0];
+    fourInRow = [0,0,0,0];
+    fourInLine = [];
+    chipWinnerCollection = [];
+	board = [
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[1,1,1,1,1,1,1],
+	];
+	stage = new Stage();
+	chips = new ChipCollection();
+	mainLoop();
+}
 
 
 //UTILITIES
